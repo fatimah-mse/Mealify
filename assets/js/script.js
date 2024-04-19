82c060474ce353bd3a5bbd0f386f9290582d1380
fatimah-mse <fatimahmse83@gmail.com>
2024-04-19 10:45:02 +0200

    modify

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+let nav= document.getElementById('to-top');
 let map = document.getElementById('map');
 let text = document.getElementById('text');
 
@@ -6,4 +7,15 @@ function show () {
     .35850965675029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1523fe3b216d9809%3A0x5f2b01a0fb9029d1!2sAl%
     20Jarkas%20Neighborhood%2C%20Jableh%2C%20Syria!5e0!3m2!1sen!2s!4v1707920354699!5m2!1sen!2s`;
     text.style.display = 'none';
+}
+
+window.onscroll = function () {
+    this.scrollY > 200 ? up.classList.add('show') : up.classList.remove('show') ;
+}
+
+function Top () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 }
